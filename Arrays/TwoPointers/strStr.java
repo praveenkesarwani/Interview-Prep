3047de35a65de5e12fa30d67512b4dbce942b01d
Praveen <pk99411@gmail.com>
2020-05-11 10:55:01 +0530

    Implement strStr()

--- Arrays/TwoPointers/strStr.java
+++ Arrays/TwoPointers/strStr.java
@@ -1,4 +1,24 @@
 //Implement strStr()
-public class strStr {
-    
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
 }
+/*
+cpp solution
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.size()==0){
+            return 0;
+        }
+        size_t found = haystack.find(needle);
+        if(found!=string::npos){
+            return found;
+        }
+        else{
+            return -1;
+        }
+    }
+};
+*/
