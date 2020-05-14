caa7eba7860bac3036296bb1273a81dfd04ea259
Praveen <pk99411@gmail.com>
2020-05-14 21:07:35 +0530

    Added more Questions

---
+++ Recursion/moduloUsingRecursion.java
@@ -0,0 +1,27 @@
+/*
+*You are given two numbers a and b. 
+*You have to find the remainder when a is divided by b using recursion. 
+*If remainder is not possible then return -1.
+*Note: You cannot use the Modulo (%) operator.
+*/
+import java.util.*;
+
+public class ModuloUsingRecursion {
+    int moduloUsingRecursion(int dividend, int divisor) {
+        if (divisor == 0) {
+            return -1;
+        }
+        if (dividend < divisor) {
+            return dividend;
+        }
+        return moduloUsingRecursion(dividend - divisor, divisor);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dividend = scanner.nextInt();
+        int divisor = scanner.nextInt();
+        int result = new ModuloUsingRecursion().moduloUsingRecursion(dividend, divisor);
+        System.out.println(result);
+    }
+}
---
+++ Recursion/towersOfHanoi.java
@@ -0,0 +1,18 @@
+//Towers of Hanoi
+import java.util.*;
+
+public class TowerOfHanoi {
+    public static void towerOfHanoi(int n,char source,char auxiliary,char destination) {
+        if(n == 0){
+			return;
+		}
+		towerOfHanoi(n-1,source,destination,auxiliary);
+		System.out.println(n+" "+source +" " +destination);
+		towerOfHanoi(n-1,auxiliary,source,destination);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        towerOfHanoi(n,'A','B','C');
+    }
+}
