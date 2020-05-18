53dab4c6c3c179755c775ee88e912c94737e92d0
Praveen <pk99411@gmail.com>
2020-05-18 20:30:21 +0530

    Trapping Rain Water

--- Arrays/TwoPointers/trapingRainWater.java
+++ Arrays/TwoPointers/trapingRainWater.java
@@ -1,4 +1,32 @@
-//Trapping rain water
-public class trap {
-    
+// Trapping Rain Water
+class Solution {
+    public int trap(int[] height) {
+        int left = 0; 
+        int right = height.length - 1;
+        int left_max = 0;
+        int right_max = 0;
+        int ans = 0;
+        while(left < right){
+            if(height[left] < height[right]){
+                if(height[left] >= left_max){
+                    left_max = height[left];
+                } else{
+                    ans += (left_max -height[left]);
+                }
+                left++;
+            } else{
+                if(height[right] >= right_max){
+                    right_max = height[right];
+                } else{
+                    ans += ((right_max - height[right]));
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
 }
+/*
+Time complexity: O(n) .
+Space complexity: O(1).
+*/
