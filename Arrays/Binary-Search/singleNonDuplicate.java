50280cab54c2741ae18897105858682536b61430
Praveen <pk99411@gmail.com>
2020-05-19 18:26:35 +0530

    Single Element in a Sorted Array

--- Arrays/Binary-Search/singleNonDuplicate.java
+++ Arrays/Binary-Search/singleNonDuplicate.java
@@ -2,3 +2,23 @@
 Find the element that occurs once in a sorted 
 array where all other elements occur twice
 */
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = left + (right - left ) / 2;
+            if( nums[mid] == nums[mid ^ 1] )
+			left = mid + 1;
+		else
+			right = mid;
+        }
+        return nums[left];
+    }
+}
+//The unique number must be at even position.
+//nums[mid] == nums[mid ^ 1], for odd position compares with the previous number and
+//for even position compares with the next number.
+//Time Complexicity: O(log n)
+//Space Complexicity: O(1)
