acd5211be6ad7696d0d7ef03c6e1ce7944b4d695
Praveen <pk99411@gmail.com>
2020-05-19 22:47:35 +0530

    two sum II

--- Arrays/Binary-Search/twoSumII.java
+++ Arrays/Binary-Search/twoSumII.java
@@ -2,30 +2,21 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] ans = new int[2];
-        for (int i = 0; i < numbers.length; i++) {
-            int newTarget = target - numbers[i];
-            int index = binarySearch(numbers, newTarget, i + 1);
-            if (index != -1) {
-                ans[0] = i + 1;
-                ans[1] = index;
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left < right){
+            if(numbers[left] + numbers[right] == target){
+                ans[0] = left + 1;
+                ans[1] = right + 1;
                 return ans;
+            }
+            else if(numbers[left] + numbers[right] < target){
+                left++;
+            }
+            else{
+                right--;
             }
         }
         return ans;
-    }
-
-    public int binarySearch(int[] arr, int target, int left) {
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (arr[mid] == target) {
-                return mid + 1;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
     }
 }
