9e86b367b4b40a2c8fb26247f4421ab7a36a68cd
Praveen <pk99411@gmail.com>
2020-05-19 09:52:33 +0530

    Peak Element in Array

--- Arrays/Binary-Search/findPeakElement.java
+++ Arrays/Binary-Search/findPeakElement.java
@@ -1,2 +1,23 @@
-//Find the maximum value in a given Bitonic array 
-//(array which first increases then decreases and all elements are unique)
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid1 = 0;
+        int mid2 = 0;
+        while(left < right){
+            mid1 = left + (right - left ) / 2;
+            mid2 = mid1 + 1;
+            if(nums[mid1] < nums[mid2]){
+                left = mid2;
+            }
+            else{
+                right = mid1;
+            }
+        }
+        return left;
+    }
+}
+/*
+Time Complexity: O(logn)
+Space Complexity: O(1)
+*/
