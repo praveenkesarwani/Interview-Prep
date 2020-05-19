70176943c38758d6899b8f2d08463be1e23c0695
Praveen <pk99411@gmail.com>
2020-05-19 18:22:47 +0530

    smallest letter greater than target

--- Arrays/Binary-Search/nextGreatestLetter.java
+++ Arrays/Binary-Search/nextGreatestLetter.java
@@ -1,2 +1,22 @@
 //Given an array of lowercase letters sorted in ascending order, 
 //find the smallest letter in the given array greater than a given key
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length;
+        int n = letters.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return letters[left % (n)];
+    }
+}
+/*
+ * Time Complexity: O(logN), where N is the length of letters. Space Complexity:
+ * O(1), as we maintain only pointers.
+ */
