7bf7feb83beb9240aeb154a7462b690e95aaf3f7
Praveen <pk99411@gmail.com>
2020-05-21 10:49:11 +0530

    700. Search in a Binary Search Tree

---
+++ BinaryTree/BST/700. Search in a Binary Search Tree.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left
+ * = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
