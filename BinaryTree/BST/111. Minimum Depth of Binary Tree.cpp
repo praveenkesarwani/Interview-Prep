92ef1d524e2c37fb107545aabde9475c842a42b9
Praveen <pk99411@gmail.com>
2020-05-22 11:00:38 +0530

    DFS

--- BinaryTree/BST/111. Minimum Depth of Binary Tree.cpp
+++ BinaryTree/BST/111. Minimum Depth of Binary Tree.cpp
@@ -6,22 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int minDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-        int leftDepth = minDepth(root->left);
-        int rightDepth = minDepth(root->right);
-        if(leftDepth == 0 || rightDepth == 0){
-            return 1 + max(leftDepth,rightDepth);
-        }
-        else{
-            return 1 + min(leftDepth,rightDepth);
-        }
+  int minDepth(TreeNode *root) {
+    if (root == NULL) {
+      return 0;
     }
+    int leftDepth = minDepth(root->left);
+    int rightDepth = minDepth(root->right);
+    if (leftDepth == 0 || rightDepth == 0) {
+      return 1 + max(leftDepth, rightDepth);
+    } else {
+      return 1 + min(leftDepth, rightDepth);
+    }
+  }
 };
