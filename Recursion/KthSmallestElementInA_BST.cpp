1d59f163b3ad7e7f3b50f6517cc62888c0321d40
Praveen <pk99411@gmail.com>
2020-05-24 10:52:19 +0530

    DFS Inorder Traversal

---
+++ Recursion/KthSmallestElementInA_BST.cpp
@@ -0,0 +1,28 @@
+/*
+// Definition for TreeNode
+class TreeNode {
+public:
+    long val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode (long x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+    stack<TreeNode*> st; 
+    int kthSmallest(TreeNode* root, int k) {
+        // base case
+        if(root == NULL)
+            return 0;
+        //Inorder traversal left->root->right
+        int left = kthSmallest(root->left, k);
+        if(st.size() == k)
+            return st.top()->val;
+        st.push(root);
+        int right = kthSmallest(root->right, k);
+
+        return st.top()->val;
+    }
