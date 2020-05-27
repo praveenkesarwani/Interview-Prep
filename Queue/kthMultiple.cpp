dd5603582ecd1a6bc70d8869fad31cb2df579953
Praveen <pk99411@gmail.com>
2020-05-27 08:53:32 +0530

    Queue Important Questions

--- Queue/kthMultiple.cpp
+++ Queue/kthMultiple.cpp
@@ -1,22 +1,35 @@
+/*
+Write an algorithm to find the Kth element in the array such that their only prime factors are 3,5 an 7. Note that 3,5, 
+and 7 do not have to be factors, but it should not have any other prime factors.
+
+Example: Such an array would contain 1,3,5,7,9,15,21,25,…
+
+We have to return the Kth element in the array.
+input:6
+output:15
+*/
 #include <bits/stdc++.h>
 using namespace std;
-long long minimum(long long x, long long y){
-    return x<y?x:y;
+long long minimum(long long x, long long y)
+{
+    return x < y ? x : y;
 }
-long long kthMultiple(long long k) {
+long long kthMultiple(long long k)
+{
     long long x = 1;
-    queue<long long int> q3,q5,q7;
-    for(int i = 1; i < k; i++){
+    queue<long long int> q3, q5, q7;
+    for (int i = 1; i < k; i++)
+    {
         q3.push(x * 3);
         q5.push(x * 5);
         q7.push(x * 7);
         x = minimum(q3.front(), (q5.front()));
         x = minimum(x, q7.front());
-        if(x == q3.front())
+        if (x == q3.front())
             q3.pop();
-        if(x == q5.front())
+        if (x == q5.front())
             q5.pop();
-        if(x == q7.front())
+        if (x == q7.front())
             q7.pop();
     }
     return x;
--- Queue/zigzagLevelOrder.java
+++ Queue/zigzagLevelOrder.java
@@ -1,3 +1,9 @@
+
+/**
+Given a binary tree, return the zigzag level order traversal of its nodes' values.
+(i.e. from left to right, then right to left for the next level and alternate for every level).
+ */
+
 import java.util.*;
 import crio.ds.Tree.TreeNode;
 
@@ -17,26 +23,26 @@ public class TreeNode {
 */
 public class Solution {
 
-    public List<List<Long>>  zigzagLevelOrder(TreeNode root) {
+    public List<List<Long>> zigzagLevelOrder(TreeNode root) {
         List<List<Long>> result = new ArrayList<List<Long>>();
-        if(root == null)
+        if (root == null)
             return result;
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.offer(root);
         boolean from_left = true;
-        while(!queue.isEmpty()){
-            //We will have all next level child nodes in the queue
+        while (!queue.isEmpty()) {
+            // We will have all next level child nodes in the queue
             int num_level_nodes = queue.size();
             List<Long> curr_level = new LinkedList<Long>();
-            for(int i = 0; i < num_level_nodes;i++){
+            for (int i = 0; i < num_level_nodes; i++) {
                 TreeNode curr_node = queue.poll();
-                if(from_left)
+                if (from_left)
                     curr_level.add(curr_node.val);
                 else
-                    curr_level.add(0,curr_node.val); //Insert at other end
-                if(curr_node.left != null)  
+                    curr_level.add(0, curr_node.val); // Insert at other end
+                if (curr_node.left != null)
                     queue.offer(curr_node.left);
-                if(curr_node.right != null)
+                if (curr_node.right != null)
                     queue.offer(curr_node.right);
             }
             result.add(curr_level);
