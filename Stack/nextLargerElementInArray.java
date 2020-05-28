c7ea3aece0d860a14ffa7cac408ce3256f34b39e
Praveen <pk99411@gmail.com>
2020-05-28 20:51:09 +0530

    Stack Important Questions

--- Stack/nextLargerElementInArray.java
+++ Stack/nextLargerElementInArray.java
@@ -1,21 +1,24 @@
 import java.util.*;
+
 class NextLargerElement {
 
-	static int [] nextLargerElement(int ar[], int n) {
+	static int[] nextLargerElement(int ar[], int n) {
 		Stack<Integer> stack = new Stack<>();
-        int [] ans = new int[n];	
-		for(int i = 0;i < n;i++){
-            // If the incoming element is greater, then it is the next greater element for the top stack element.
-            // We pop this element from the stack.
-			while(!stack.empty() && ar[i] > ar[stack.peek()]){
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			// If the incoming element is greater, then it is the next greater element for
+			// the top stack element.
+			// We pop this element from the stack.
+			while (!stack.empty() && ar[i] > ar[stack.peek()]) {
 				ans[stack.peek()] = ar[i];
 				stack.pop();
-            }
-            //If the Stack is empty (which it is initially), then push the element in it.
+			}
+			// If the Stack is empty (which it is initially), then push the element in it.
 			stack.push(i);
-        }
-        // when all the array elements have been traversed, for the remaining elements in the Stack, we can simply add -1.
-		while(!stack.empty()){
+		}
+		// when all the array elements have been traversed, for the remaining elements
+		// in the Stack, we can simply add -1.
+		while (!stack.empty()) {
 			ans[stack.peek()] = -1;
 			stack.pop();
 		}
@@ -29,7 +32,7 @@ class NextLargerElement {
 		for (int i = 0; i < n; i++) {
 			ar[i] = sc.nextInt();
 		}
-		int [] ans = nextLargerElement(ar, n);
+		int[] ans = nextLargerElement(ar, n);
 		for (int i = 0; i < ans.length; i++)
 			System.out.print(ans[i] + " ");
 	}
