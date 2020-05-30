0b63d9648930ab26673ab0493f350093d0f7f331
Praveen <pk99411@gmail.com>
2020-05-30 17:58:08 +0530

    minHeap

--- Hash&heap/Find a way to connect N ropes with minimum cost.cpp
+++ Hash&heap/Find a way to connect N ropes with minimum cost.cpp
@@ -7,15 +7,12 @@ Your goal is to connect all N ropes together to form a single rope and minimize 
 long long connectRopes(vector<long long > arr)
 {
     long long cost = 0;
-    //edge case
-    if(arr.size() == 1)
-        return cost;
     //create min_heap
     priority_queue<long long, vector<long long>,greater<long long>> pq;
     // push all elements of arr into min_heap
     for(int rope: arr)
         pq.push(rope);
-    while(!pq.empty()){
+    while(pq.empty() > 1){
         //pop two elements from pq and add them
         long long rope1 = pq.top(); 
         pq.pop();
@@ -23,9 +20,7 @@ long long connectRopes(vector<long long > arr)
         pq.pop();
         long long sum = rope1 + rope2;
         cost += sum;
-        //when all elements are popped
-        if(!pq.empty())
-            pq.push(sum);
+        pq.push(sum);
     }
     return cost;
 }
