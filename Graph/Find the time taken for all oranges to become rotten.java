4ef5bc20f1a247191be3f1176435274d06846bc7
Praveen Kesarwani <praveenkesarwani739@gmail.com>
2020-07-17 10:45:02 +0530

    Updated

--- Graph/Find the time taken for all oranges to become rotten.java
+++ Graph/Find the time taken for all oranges to become rotten.java
@@ -1,3 +1,8 @@
+/*
+--------------------------------------------------------------------------------------------------------
+|                                        1st Method                                                               |
+--------------------------------------------------------------------------------------------------------
+*/
 import java.util.*;
 
 class RottingOranges {
@@ -73,3 +78,104 @@ class RottingOranges {
         return -1;
     }
 }
+/*
+--------------------------------------------------------------------------------------------------------
+|                                        2nd Method                                                               |
+--------------------------------------------------------------------------------------------------------
+*/
+import java.util.*;
+
+class RottingOranges{
+    static class Pair{
+        int first;
+        int second;
+        Pair(int first,int second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+    static int freshOranges;
+    static Queue<Pair> q;
+    static int times;
+    static int rows;
+    static int cols;
+    static int []dir = {-1,0,1,0,-1};
+    public static int rottingOranges(int[][]grid){
+        if(grid == null || grid[0].length == 0){
+            return 0;
+        }
+        rows = grid.length;
+        cols = grid[0].length;
+        q = new LinkedList<>();
+        freshOranges = 0;
+        times = 0;
+        for(int i = 0;i < rows;i++){
+            for(int j = 0;j < cols;j++){
+                if(grid[i][j] == 2){
+                    Pair pair = new Pair(i,j);
+                    q.offer(pair);
+                }
+                if(grid[i][j] == 1){
+                    freshOranges++;
+                }
+            }
+        }
+        BFS(grid);
+        return freshOranges == 0 ? times : -1;
+    }
+
+    public static void BFS(int [][]grid){
+        boolean checked = false;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int j = 0;j < size;j++){
+                Pair coordinates = q.poll();
+                int x_coord = coordinates.first;
+                int y_coord = coordinates.second;
+                for(int i = 0;i < 4;i++){
+                    int x = x_coord + dir[i];
+                    int y = y_coord + dir[i + 1];
+                    if(isValid(x,y,grid)){
+                        grid[x][y] = 2;
+                        freshOranges--;
+                        Pair pair = new Pair(x,y);
+                        q.offer(pair);
+                        checked = true;
+                    }
+                }
+            }
+            if(checked){
+                times++;
+                checked = !checked;
+            }
+        }
+    }
+    public static boolean isValid(int x,int y,int[][] grid){
+        if(x < 0 || x >= rows || y < 0 || y >= cols){
+            return false;
+        }
+        if(grid[x][y] == 0 || grid[x][y] == 2){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while((t--)>0){
+            int n,m;
+            n= sc.nextInt();
+            m=sc.nextInt();
+            int[][]grid = new int[n][m];
+            for(int i=0;i<n;i++){
+                for(int j = 0;j<m;j++){
+                    grid[i][j] = sc.nextInt();
+                }
+            }
+            int ans  = RottingOranges.rottingOranges(grid);
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+} 
